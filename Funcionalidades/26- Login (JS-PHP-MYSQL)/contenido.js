347b0cda9d61cfc9be1199641b6b8dc971c8ba6f
Harold Valdés <hardevkoder@gmail.com>
2025-10-10 12:15:18 -0500

    Actualizo Repo

--- Funcionalidades/26- Login (JS-PHP-MYSQL)/contenido.js
+++ Funcionalidades/26- Login (JS-PHP-MYSQL)/contenido.js
@@ -4,6 +4,8 @@
 // Referencio elementos del DOM
 //===============================================================
 const spanNombres = document.getElementById("spanNombres");
+const spanRegistrosUsuario = document.getElementById("spanRegistrosUsuario");
+const spanRegistrosSistema = document.getElementById("spanRegistrosSistema");
 
 //===============================================================
 // Función que extrae datos del Local Storage
@@ -30,6 +32,8 @@ const leerDeLS = (clave) => {
 const datosDeUsuario = leerDeLS("datosUsuarioLogueado");
 
 //===============================================================
-// Muestro Nombres del usuario en la pagina de contenido
+// Muestro Datos del usuario en la pagina de contenido
 //===============================================================
 spanNombres.textContent = datosDeUsuario.nombresDelUsuario;
+spanRegistrosUsuario.textContent = datosDeUsuario.registrosDelUsuario;
+spanRegistrosSistema.textContent = datosDeUsuario.registrosAcumuladosSistema;
